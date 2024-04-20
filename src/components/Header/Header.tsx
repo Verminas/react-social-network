8ee183ad927c997e245d25e8c6a052e26a7f4700
Verminas <veremeeva96@gmail.com>
2024-04-20 20:32:48 +0300

    refactor: styles for different components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Header.css";
 
 type HeaderPropsType = {
   className: string
 }
 export const Header = (props: HeaderPropsType) => {
   return (
-    <div className={'header'}>
+    <header className={'header'}>
       <img className={'header__logo'}
            src="https://cdn.icon-icons.com/icons2/838/PNG/512/circle-dribble_icon-icons.com_66836.png" alt="logo"/>
-    </div>
+    </header>
   )
 }
