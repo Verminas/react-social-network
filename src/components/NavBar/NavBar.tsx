7b3c1e42935d4c14e30e38ceeddf6e9f702cd818
Verminas <veremeeva96@gmail.com>
2024-04-22 14:34:51 +0300

    feat: use navlink instead a in navbar component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import s from "./NavBar.module.css";
+import {NavLink} from "react-router-dom";
 
 export const NavBar = () => {
   return (
     <nav className={s.navBar}>
-      <a href="/profile">Profile</a>
-      <a href="/dialogs">Message</a>
-      <a href="/news">News</a>
-      <a href="/music">Music</a>
-      <a href="/settings">Settings</a>
+      <NavLink to="/profile" className={({ isActive}) => isActive ? s.activeLink : ''}>Profile</NavLink>
+      <NavLink to="/dialogs" className={({ isActive}) => isActive ? s.activeLink : ''}>Message</NavLink>
+      <NavLink to="/news" className={({ isActive}) => isActive ? s.activeLink : ''}>News</NavLink>
+      <NavLink to="/music" className={({ isActive}) => isActive ? s.activeLink : ''}>Music</NavLink>
+      <NavLink to="/settings" className={({ isActive}) => isActive ? s.activeLink : ''}>Settings</NavLink>
     </nav>
   )
 }
