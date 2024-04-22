60f0e41e954d73232c672f252281414c07fafe0c
Verminas <veremeeva96@gmail.com>
2024-04-22 15:06:47 +0300

    feat: add messages in dialogs. add basic styles

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,22 @@ import s from './Dialogs.module.css';
 
 export const Dialogs = () => {
   return (
-    <section>
-      Dialogs
+    <section className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <h2 className={s.title}>Dialogs</h2>
+        <ul>
+          <li><a href="" className={s.dialog}>Dmitry</a></li>
+          <li><a href="" className={s.dialog}>Sveta</a></li>
+          <li><a href="" className={s.dialog}>Valera</a></li>
+          <li><a href="" className={s.dialog}>Alex</a></li>
+          <li><a href="" className={s.dialog}>Natali</a></li>
+        </ul>
+      </div>
+      <div className={s.messages}>
+        <p className={s.message}>Hello</p>
+        <p className={s.message}>How are you</p>
+        <p className={s.message}>I'm fine</p>
+      </div>
     </section>
   )
 }
