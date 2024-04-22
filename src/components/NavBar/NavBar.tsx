04fa8492415f5973bd9202e21798a042ee446be4
Verminas <veremeeva96@gmail.com>
2024-04-23 01:01:12 +0300

    feat: add aside next to navbar for friends

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,12 +4,23 @@ import {NavLink} from "react-router-dom";
 
 export const NavBar = () => {
   return (
-    <nav className={s.navBar}>
-      <NavLink to="/profile" className={({ isActive}) => isActive ? s.activeLink : ''}>Profile</NavLink>
-      <NavLink to="/dialogs" className={({ isActive}) => isActive ? s.activeLink : ''}>Message</NavLink>
-      <NavLink to="/news" className={({ isActive}) => isActive ? s.activeLink : ''}>News</NavLink>
-      <NavLink to="/music" className={({ isActive}) => isActive ? s.activeLink : ''}>Music</NavLink>
-      <NavLink to="/settings" className={({ isActive}) => isActive ? s.activeLink : ''}>Settings</NavLink>
-    </nav>
+    <div className={s.wrapperNavAside}>
+      <nav className={s.navBar}>
+        <NavLink to="/profile" className={({isActive}) => isActive ? s.activeLink : ''}>Profile</NavLink>
+        <NavLink to="/dialogs" className={({isActive}) => isActive ? s.activeLink : ''}>Message</NavLink>
+        <NavLink to="/news" className={({isActive}) => isActive ? s.activeLink : ''}>News</NavLink>
+        <NavLink to="/music" className={({isActive}) => isActive ? s.activeLink : ''}>Music</NavLink>
+        <NavLink to="/settings" className={({isActive}) => isActive ? s.activeLink : ''}>Settings</NavLink>
+      </nav>
+      <aside className={s.aside}>
+        <h2 className={s.asideTitle}>Friends</h2>
+        <ul className={s.friendsList}>
+          <li>Sveta</li>
+          <li>Sasha</li>
+          <li>Valera</li>
+        </ul>
+      </aside>
+    </div>
+
   )
 }
