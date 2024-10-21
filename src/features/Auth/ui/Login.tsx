b414fa1676a2666674b731273019a7b520c95a5a
Verminas <veremeeva96@gmail.com>
2024-10-22 00:31:43 +0300

    feat: add free case for login

--- src/features/Auth/ui/Login.tsx
+++ src/features/Auth/ui/Login.tsx
@@ -20,6 +20,30 @@ export const Login = () => {
       variant={"outlined"}
       size="large"
     >
+      <div
+        style={{
+          fontSize: "16px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          marginBottom: "20px",
+          color: "#636363",
+        }}
+      >
+        <p>
+          To log in get registered{" "}
+          <a
+            href="https://social-network.samuraijs.com/"
+            target="_blank"
+            style={{ color: "#1677FF" }}
+          >
+            here
+          </a>
+        </p>
+        <p>or use common test account credentials: </p>
+        <p>Email: free@samuraijs.com</p>
+        <p>Password: free</p>
+      </div>
       <Form.Item<FieldType>
         name="email"
         rules={rulesEmail}
