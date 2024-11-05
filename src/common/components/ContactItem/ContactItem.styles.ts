b6608075f6a5579f7b14644a851b3688694138ab
Verminas <veremeeva96@gmail.com>
2024-11-05 20:28:51 +0300

    refactor: update contact item styles for label

--- src/common/components/ContactItem/ContactItem.styles.ts
+++ src/common/components/ContactItem/ContactItem.styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { theme } from "common/styles/theme";
 
-const Label = styled.p`
+const Label = styled.span`
+    display: block;
     color: ${theme.colors.textSecondary};
     line-height: 1.5;
     font-size: clamp(14px, 3vw, 18px);
