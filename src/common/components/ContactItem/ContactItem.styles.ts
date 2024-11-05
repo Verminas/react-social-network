6920beb89b059d63401c87497b35e15361784d5f
Verminas <veremeeva96@gmail.com>
2024-11-05 18:49:18 +0300

    refactor: ContactItem styles

--- src/common/components/ContactItem/ContactItem.styles.ts
+++ src/common/components/ContactItem/ContactItem.styles.ts
@@ -1,21 +1,20 @@
 import styled from "styled-components";
-import {theme} from "common/styles/theme";
+import { theme } from "common/styles/theme";
 
-const Label = styled.span`
-  display: block;
-  color: ${theme.colors.textSecondary};
-  line-height: 1.5;
-  font-size: 18px;
-  text-transform: capitalize;
+const Label = styled.p`
+    color: ${theme.colors.textSecondary};
+    line-height: 1.5;
+    font-size: clamp(14px, 3vw, 18px);
+    text-transform: capitalize;
 `;
 
 const Link = styled.a`
-  color: ${theme.colors.textMain};
-  line-height: 1.5;
-  font-size: 18px;
-  text-transform: lowercase;
+    color: ${theme.colors.textMain};
+    text-transform: lowercase;
+    word-wrap: break-word; /* Для поддержки старых браузеров */
+    overflow-wrap: break-word; /* Для современных браузеров */
 `;
 
 export const S = {
-    Link, Label
-}
+  Link, Label
+};
