593755020b1a9b2aaab9db7aced648867dcdc2c6
Verminas <veremeeva96@gmail.com>
2024-11-09 20:06:01 +0300

    refactor: styles for posts in profile

---
+++ src/features/SocialNetwork/ui/Layout/Profile/Posts/Posts.styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { theme } from "common/styles/theme";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    padding-top: 30px;
+`
+
+const Form = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`
+
+const Title = styled.h3`
+    color: ${theme.colors.textMain}
+`
+
+const Posts = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
+
+export const S = {
+  Wrapper, Form, Title, Posts
+};
--- src/features/SocialNetwork/ui/Layout/Profile/Posts/Posts.tsx
+++ src/features/SocialNetwork/ui/Layout/Profile/Posts/Posts.tsx
@@ -1,24 +1,24 @@
-import s from "features/SocialNetwork/ui/Layout/Profile/Posts/Posts.module.css";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { SubmitForm } from "common/components/SubmitForm/SubmitForm";
 import { UserContext } from "features/SocialNetwork/ui/Layout/Profile/Profile";
+import {S} from "./Posts.styles"
 
 export const Posts = () => {
-  const { user, isAuthUser } = useContext(UserContext);
+  const {isAuthUser } = useContext(UserContext);
   const onClickHandler = (value: string) => {
     alert(value);
   };
 
   return (
-    <div className={s.posts}>
-      <div className={s.myPost}>
-        <h2 className={s.myPostTitle}>{isAuthUser ? "My posts" : "Posts"}</h2>
+    <S.Wrapper>
+      <S.Form>
+        <S.Title>{isAuthUser ? "My posts" : "Posts"}</S.Title>
         <SubmitForm
           onSubmitForm={onClickHandler}
           placeholder={"your news..."}
         />
-      </div>
-      <div className={s.postsItems}></div>
-    </div>
+      </S.Form>
+      <S.Posts></S.Posts>
+    </S.Wrapper>
   );
 };
